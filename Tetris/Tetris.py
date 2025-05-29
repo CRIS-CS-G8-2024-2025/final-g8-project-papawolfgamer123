21edee6b4f1d98f1ca9c209ec888787b2581a08d
Phoom <phoom.p@cris.ac.th>
2025-05-29 08:18:21 +0700

    Updated read me file with description and purpose. Updated main.py file too

---
+++ Tetris/Tetris.py
@@ -0,0 +1,28 @@
+from settings import *
+from Tetris import Tetris
+import sys
+
+class App:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption('Tetris')
+        self.screen = pg.display.set_mode(FIELD_RES)
+        self.clock = pg.time.Clock()
+        self.tetris = Tetris(self)
+
+    def update(self):
+        self.tetris.update()
+        self.clock.tick(FPS)
+
+    def draw(self):
+        self.screen.fill(color=FIELD_COLOR)
+        self.tetris.draw()
+        pg.display.flip()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+                
+    
---
+++ Tetris/main.py
@@ -0,0 +1,10 @@
+from settings import *
+from Tetris import Tetris
+import sys
+
+class App:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption('Tetris')
+        self.screen
+    
